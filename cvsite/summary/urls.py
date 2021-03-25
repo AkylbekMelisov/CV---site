1ea6897260666e42b86dfa1a341eb37fba861f5d
Melisov Akylbek <akylbek.melisov@gmail.com>
2021-03-25 16:26:28 +0600

    create urls Education, Skill and Language

--- cvsite/summary/urls.py
+++ cvsite/summary/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from .views import *
 
-
-urlpatterns = []
+urlpatterns = [
+    path('', ResumeView.as_view()),
+    path('experience/<int:resume_id>/', ExperienceView.as_view()),
+    path('education/<int:resume_id>/', EducationView.as_view()),
+    path('skill/<int:resume_id>/', SkillView.as_view()),
+    path('language/<int:resume_id>/', LanguageView.as_view())
+]
